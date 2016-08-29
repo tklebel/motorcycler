de6ffbda518270f2d601d86872f940982b030bde
Thomas Klebel <tklebel@users.noreply.github.com>
2016-08-29 11:44:47 +0200

    combine data

---
+++ analysis/combine_data.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(readr)
+library(purrr)
+
+files <- list.files("data-chunks/", full.names = T)
+
+combined_data <- map(files, read_rds) %>%
+  bind_rows()
+
+write_rds(combined_data, "data-combined/combined_data.rds")
+
